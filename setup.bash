3de116efa7f5fc7209d85631133db493ede3cf8f
Roneil Rumburg <me@roneil.com>
2014-07-19 15:37:38 -0700

    Improved setup script

--- setup.bash
+++ setup.bash
@@ -1,22 +1,66 @@
 #!/bin/bash
 
-# Bootstrap vim plugin manager
-if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
-  mkdir -p ~/.vim/bundle
-  git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+#set -o xtrace
+set -e
+
+function install_ask {
+  # $1 = package to ask about installing
+  read -p "Would you like to install extras for $1? (y/n) " -n 1 -r
+  echo
+  [[ $REPLY =~ ^[Yy]$ ]]
+}
+
+if install_ask "bash"; then
+  stow bash
 fi
 
-# create .gitconfig-local if it doesn't exist
-if [ ! -e ~/.gitconfig-local ]; then
-  echo "[user]
-  name = Example person
-  email = example@example.com" > ~/.gitconfig-local
+if install_ask "git"; then
+  # create .gitconfig-local if it doesn't exist
+  if [ ! -e ~/.gitconfig-local ]; then
+    echo "[user]
+    name = Example person
+    email = example@example.com" > ~/.gitconfig-local
 
-  echo "Edit local configuration options (name, email, etc.) in ~/.giconfig-local"
+    echo "Edit local configuration options (name, email, etc.) in ~/.giconfig-local"
+  fi
+
+  stow git
 fi
 
-# Symlink dotfiles into place
-stow bash git ssh tmux vim
+if install_ask "osx"; then
+  ./osx/defaults.sh
 
-# install vim plugins
-vim +NeoBundleInstall +q
+  if install_ask "enable trim"; then
+    ./osx/trim_enabler.sh
+  fi
+
+  if install_ask "fonts"; then
+    open osx/fonts/*
+  fi
+
+  if install_ask "iterm"; then
+    open osx/iterm/*
+  fi
+fi
+
+if install_ask "ssh"; then
+  stow ssh
+fi
+
+if install_ask "tmux"; then
+  stow tmux
+fi
+
+if install_ask "vim"; then
+  # Bootstrap vim plugin manager
+  if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
+    mkdir -p ~/.vim/bundle
+    git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+  fi
+
+  # install dotfiles for vim
+  stow vim
+
+  # install vim plugins
+  vim +NeoBundleInstall +q
+fi
